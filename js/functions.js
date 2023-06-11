cfe251d06a75ea4fd54178bf5504de19ed0f0dab
YuliaKoltsova <yuliykoo@mail.ru>
2023-06-11 20:44:15 +0400

    Исправляет ошибки ESLint

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,30 @@
 // Функция для проверки длины строки
-let checkLengthString = function (string, maxLength) {
+const checkLengthString = function (string, maxLength) {
   if (string.length <= maxLength) {
     return true;
   }
   return false;
 };
 
-console.log (checkLengthString ('проверяемая строка', 20));
-console.log (checkLengthString ('проверяемая строка', 18));
-console.log (checkLengthString ('проверяемая строка', 10));
+checkLengthString ('проверяемая строка', 20);
+checkLengthString ('проверяемая строка', 18);
+checkLengthString ('проверяемая строка', 10);
 
 // Функция для проверки, является ли строка палиндромом
-let checkPalindrome = function (string) {
-  let withoutSpaces = string.replaceAll(' ', '');
-  let normalizeString = withoutSpaces.toUpperCase();
+const checkPalindrome = function (string) {
+  const withoutSpaces = string.replaceAll(' ', '');
+  const normalizeString = withoutSpaces.toUpperCase();
   let invertedString = '';
   for (let i = normalizeString.length - 1; i >= 0; i--) {
     invertedString += normalizeString.at(i);
-  };
+  }
   if (invertedString === normalizeString) {
     return true;
-  };
+  }
   return false;
-}
+};
 
-console.log(checkPalindrome('топот'));
-console.log(checkPalindrome('ДовОд'));
-console.log(checkPalindrome('Кекс'));
-console.log(checkPalindrome('Лёша на полке клопа нашёл'));
+checkPalindrome('топот');
+checkPalindrome('ДовОд');
+checkPalindrome('Кекс');
+checkPalindrome('Лёша на полке клопа нашёл');
