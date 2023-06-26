87082fcd5fde95bd833bafda53e6a903970df07e
Юлия Кольцова <yuliykoo@mail.ru>
2023-06-26 16:55:07 +0400

    Исправление в описании функции

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,7 @@ const isWorkingDay = (startWork, endWork, startMeeting, durationMeeting) => {
   // Считаем сколько минут от начала рабочего дня до встречи (если встреча началась до начала рабочего дня)
   const minutesStartWork = (Number(timeStartMeeting[0]) * 60 + Number(timeStartMeeting[1])) - (Number(timeStartWork[0] * 60) + Number(timeStartWork[1]));
   // Проверяем выходит ли встреча за рамки рабочего дня
-  // Если количество минут до конца рабочего дня >= количеству минут встречи или количество минут от начала рабочего дня до начала встречи >=0
+  // Если количество минут до конца рабочего дня >= количеству минут встречи и количество минут от начала рабочего дня до начала встречи >=0
   // то встрче началась в рабочий день => true
   if (minutesEndWork >= durationMeeting && minutesStartWork >= 0) {
     return true;
