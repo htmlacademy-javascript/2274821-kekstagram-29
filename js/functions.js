f1c8fb9c7fa62b47f694c3b3b762c8982b5f87c9
Юлия Кольцова <yuliykoo@mail.ru>
2023-06-26 16:47:14 +0400

    Убирает ненужные функции

--- js/functions.js
+++ js/functions.js
@@ -18,28 +18,3 @@ const isWorkingDay = (startWork, endWork, startMeeting, durationMeeting) => {
 isWorkingDay('7:00', '18:00', '17:15', 46);
 // Проверка
 // console.log(isWorkingDay('7:00', '18:00', '17:15', 45));
-
-
-// // Функция для проверки длины строки
-// const checkLengthString = function (string, maxLength) {
-//   return string.length <= maxLength;
-// };
-
-// checkLengthString ('проверяемая строка', 20);
-// checkLengthString ('проверяемая строка', 18);
-// checkLengthString ('проверяемая строка', 10);
-
-// // Функция для проверки, является ли строка палиндромом
-// const checkPalindrome = function (string) {
-//   const normalizeString = string.replaceAll(' ', '').toUpperCase();
-//   let invertedString = '';
-//   for (let i = normalizeString.length - 1; i >= 0; i--) {
-//     invertedString += normalizeString.at(i);
-//   }
-//   return invertedString === normalizeString;
-// };
-
-// checkPalindrome('топот');
-// checkPalindrome('ДовОд');
-// checkPalindrome('Кекс');
-// checkPalindrome('Лёша на полке клопа нашёл');
