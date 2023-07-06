ae93203a92a2ac6dad28af95377160c6d3312f29
Юлия Кольцова <yuliykoo@mail.ru>
2023-07-06 17:45:58 +0400

    Исправляет EventListener

--- js/actions-modal.js
+++ js/actions-modal.js
@@ -10,31 +10,13 @@ const commentsLoader = document.querySelector('.comments-loader');
 const bigPhotoModal = document.querySelector('.big-picture');
 const closeBigPhotoModal = document.querySelector('.big-picture__cancel');
 
-// Функция закрытия модального окна по кнопке ESС
-const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeModal();
-  }
-};
-
 // <Закрытие модального окна>
 const closeModal = () => {
   bigPhotoModal.classList.add('hidden');
   // Так как модальное окно закрыто, обработчик нам не нужен, поэтому удаляем его
-  document.removeEventListener('keydown', onDocumentKeydown);
+  window.removeEventListener('keydown', onDocumentKeydown);
   document.body.classList.remove('modal-open');
 };
-// Закрытие модального окна по нажатию на крестик
-closeBigPhotoModal.addEventListener('click', () => {
-  closeModal();
-});
-// Закрытие модального окна по нажатию на Esc
-window.addEventListener('keydown', () => {
-  if(isEscapeKey){
-    closeModal();
-  }
-});
 
 // <Открытие модального окна>
 const openModal = () => {
@@ -45,7 +27,7 @@ const openModal = () => {
     closeModal();
   });
   // Закрытие по ESC
-  document.addEventListener('keydown', onDocumentKeydown);
+  window.addEventListener('keydown', onDocumentKeydown);
   // После открытия окна спрячьте блоки счётчика комментариев
   // .social__comment-count и загрузки новых комментариев .comments-loader, добавив им класс hidden
   socialCommentsCount.classList.add('hidden');
@@ -53,6 +35,14 @@ const openModal = () => {
   // После открытия окна добавьте тегу <body> класс modal-open, чтобы контейнер с фотографиями позади не прокручивался при скролле.
   document.body.classList.add('modal-open');
 };
+
+// Функция закрытия модального окна по кнопке ESС
+function onDocumentKeydown (evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeModal();
+  }
+}
 
 // Показывает наполненное окно по клику
 collectionPosts.addEventListener('click', (evt) => {
